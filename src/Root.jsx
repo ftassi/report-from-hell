4f95a59f4734d6e27bb61beeee2a90003463b79d
ricfrank <riccardo.franconi@gmail.com>
2017-06-20 09:01:00 +0200

    provisional commit

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+import {Router, Route, browserHistory, hashHistory, IndexRoute} from 'react-router'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import ProjectIssuesList from './pages/ProjectIssuesList.jsx'
@@ -7,7 +7,7 @@ import ProjectIssuesList from './pages/ProjectIssuesList.jsx'
 const Root = React.createClass({
     render() {
         return (
-            <Router history={browserHistory}>
+            <Router history={hashHistory}>
               <Route path='/' component={App} >
                 <IndexRoute component={Home}/>
                 <Route path='/issues/:projectId' component={ProjectIssuesList} />
